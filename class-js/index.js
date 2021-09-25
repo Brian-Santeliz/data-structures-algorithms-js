8ccd69d601406dbc6a8e2155f532436fe75292e7
Brian <blsvsanteliz@gmail.com>
2021-09-25 13:09:36 -0400

    Create example basic of Class in JS

---
+++ class-js/index.js
@@ -0,0 +1,12 @@
+class Hero {
+  constructor(name) {
+    this.name = name;
+  }
+  getHeroName() {
+    return this.name;
+  }
+  setHeroName(name) {
+    this.name = name;
+    return this.name;
+  }
+}
