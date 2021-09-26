0f4e3fc6b8ecbf202b13f7ad3ca9499335bccfe1
Brian <blsvsanteliz@gmail.com>
2021-09-26 18:41:36 -0400

    Overview of linke list in Big O

---
+++ linked-list/index.js
@@ -0,0 +1,10 @@
+/* 
+    Linked list Big O operations:
+    4. Remove in the fist node -> O(1)
+    1. Insert in the last node -> O(1)
+    2. Insert in the first node -> O(1)
+    3. Remove in the last node -> O(n) -> In array is de the best O(1) 
+    5. Insert in the middle -> O(n)
+    5. Reomve in the middle -> O(n)
+    5. Search by index -> O(n) ->   In array is de the best O(1)
+*/
